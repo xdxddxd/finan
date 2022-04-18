9686a6e928de1618ad517058c49094259882f476
Derik <ytxd.gamer@gmail.com>
2022-04-18 20:57:12 -0300

    .

--- source/views/App/Home.php
+++ source/views/App/Home.php
@@ -8,21 +8,33 @@
         <!-- Begin Page Content -->
         <div class="container-fluid">
 
-            <!-- Page Heading -->
-            <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                <h1 class="h3 mb-0 text-gray-800">Titulo da Página</h1>
-                <!--a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a-->
-            </div>
-
             <div class="row">
 
-                <div class="col-md-12">
+                <div class="col-md-6">
+                    <!-- Page Heading -->
+                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                        <h1 class="h3 mb-0 text-gray-800">Precisa de Dinheiro</h1>
+                        <!--a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a-->
+                    </div>
 
-                    Conteúdo dá Página a
+                    <div class="row">
 
+                        <div class="col-md-12">
+
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+                            standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make.
+
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <img src="<?php echo URL_BASE; ?>assets/img/tesouro.svg" alt="Tesouro">
                 </div>
 
             </div>
+
 
         </div>
 
